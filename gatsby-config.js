a8992f47b71351e9493957e59a176adc64be6cfe
SpicyMeatloaf <jordan.leewc@gmail.com>
2021-08-11 00:46:25 -0700

    Initial setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,16 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "web-portfolio",
+    title: "Portfolio",
+    author: "Jordan Lee",
+    description: "Personal website",
   },
   plugins: [
     "gatsby-plugin-styled-components",
     "gatsby-plugin-sitemap",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
